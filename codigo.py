3227e9af07caf229065c5abe4bfbb49b3238e541
checho <sergio.echeverrycla@gmail.com>
2024-05-03 18:26:02 -0500

    pruebas server

--- codigo.py
+++ codigo.py
@@ -1,46 +1,47 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
-import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, initialize_app
 
-if not firebase_admin._apps:
-    # Inicializar Firebase
-    cred = credentials.Certificate("key.json")
-    firebase_admin.initialize_app(cred)
-    db = firestore.client()
-
-# Cargar el conjunto de datos
-df = pd.read_csv('Goodreads_books_with_genres_simplificado.csv')
-df = df[df['num_pages'] != 0]
-df = df.dropna()
-# Combinar géneros y autores en una sola columna
-df['text'] = df['genres'] + ';' + df['Author']
+# Inicializar la app de Firebase
+cred = credentials.Certificate("key.json")
+initialize_app(cred)
+db = firestore.client()
 
 # Vectorización
 vectorizer = TfidfVectorizer()
-tfidf_matrix = vectorizer.fit_transform(df['text'])
 
 # Modelo de vecinos más cercanos
 model_knn = NearestNeighbors(metric='cosine', algorithm='brute')
-model_knn.fit(tfidf_matrix)
 
-def obtener_preferencias_usuario(uid):
-    # Obtener los datos del usuario desde Firestore
-    doc_ref = db.collection('users').document(uid)
-    doc = doc_ref.get().to_dict()
-    if doc:
-        genres = doc.get('genres', [])
-        authors = doc.get('authors', [])
-        print("Preferencias de géneros:", genres)
-        print("Preferencias de autores:", authors)
-        return doc.get('genres', []), doc.get('authors', [])
-    else:
-        return [], []
+def cargar_datos_usuarios():
+    usuarios = db.collection('users').stream()
+    datos_usuarios = []
+    for usuario in usuarios:
+        datos_usuario = usuario.to_dict()
+        datos_usuarios.append(datos_usuario)
+    return pd.DataFrame(datos_usuarios)
 
-def recomendar_libros(uid):
-    # Obtener las preferencias del usuario
-    generos_favoritos, autores_favoritos = obtener_preferencias_usuario(uid)
+def cargar_datos_libros_desde_csv(ruta):
+    return pd.read_csv(ruta)
+
+def recomendar_libros(generos_favoritos, autores_favoritos):
+    # Cargar los datos de los usuarios desde Firestore
+    df_usuarios = cargar_datos_usuarios()
+    
+    # Cargar los datos de los libros desde un archivo CSV
+    df_libros = cargar_datos_libros_desde_csv('Goodreads_books_with_genres_simplificado.csv')
+    df_libros = df_libros[df_libros['num_pages'] != 0]
+    df_libros = df_libros.dropna()
+    
+    # Combinar géneros y autores en una sola columna
+    df_libros['text'] = df_libros['genres'] + ';' + df_libros['Author']
+    
+    # Vectorización
+    tfidf_matrix = vectorizer.fit_transform(df_libros['text'])
+    
+    # Entrenar el modelo de vecinos más cercanos
+    model_knn.fit(tfidf_matrix)
     
     # Crear el texto de entrada del usuario
     texto_usuario = ';'.join(generos_favoritos + autores_favoritos)
@@ -52,12 +53,7 @@ def recomendar_libros(uid):
     distancias, indices = model_knn.kneighbors(vector_usuario, n_neighbors=10)
     
     # Recuperar los libros recomendados
-    libros_recomendados = df.iloc[indices.flatten()]
+    libros_recomendados = df_libros.iloc[indices.flatten()]
     
-    # Convertir el DataFrame a una lista de diccionarios
-    libros_recomendados_dict = libros_recomendados[['Title', 'Author', 'genres']].to_dict('records')
-    
-    return libros_recomendados_dict
-
-
+    return libros_recomendados[['Title', 'Author', 'genres']]
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import os
 import codigo
+from codigo import db, recomendar_libros
 
 app = Flask(__name__)
 
@@ -13,7 +14,15 @@ def hello_world():
 
 @app.route('/recomendaciones/<uid>')
 def recomendaciones(uid):
-    return codigo.recomendar_libros(uid)
+    # Obtener los géneros y autores favoritos del usuario desde Firestore
+    datos_usuario = db.collection('users').document(uid).get().to_dict()
+    generos_favoritos = datos_usuario.get('genres', [])
+    autores_favoritos = datos_usuario.get('authors', [])
+    
+    # Llamar a la función recomendar_libros con los géneros y autores favoritos
+    libros_recomendados = codigo.recomendar_libros(generos_favoritos, autores_favoritos)
+    
+    return libros_recomendados.to_json()  # Devolver los resultados como JSON
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=int(os.environ.get('PORT', 5000)),debug=False)
